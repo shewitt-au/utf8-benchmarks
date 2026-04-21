a593a41e34eb90081167cbd5ca55e0ed11f5f497
shewitt.au <shewitt.au@gmail.com>
2026-04-22 01:51:09 +1000

    Add a poem in 3 langs and the tests

--- bench.cpp
+++ bench.cpp
@@ -9,30 +9,104 @@
 
 static const std::string test_string = make_random_utf8(1024);
 
-static void BM_Steve_strlenUtf8_Traditional(benchmark::State& state) {
+static void BM_Steve_strlenUtf8_Traditional_Random(benchmark::State& state) {
     for (auto _ : state) {
         auto result = steve::strlenUtf8_Traditional(test_string);
         benchmark::DoNotOptimize(result);
     }
 }
 
-static void BM_Steve_StrlenUtf8(benchmark::State& state) {
+static void BM_Steve_strlenUtf8_Traditional_PoemEnglish(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = steve::strlenUtf8_Traditional(g_poemEnglish);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_Steve_strlenUtf8_Traditional_PoemChinese(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = steve::strlenUtf8_Traditional(g_poemChinese);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_Steve_strlenUtf8_Traditional_PoemRussian(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = steve::strlenUtf8_Traditional(g_poemRussian);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_Steve_strlenUtf8_Random(benchmark::State& state) {
     for (auto _ : state) {
         auto result = steve::strlenUtf8(test_string);
         benchmark::DoNotOptimize(result);
     }
 }
 
-static void BM_AxCut_utf8StringLength(benchmark::State& state) {
+static void BM_Steve_strlenUtf8_PoemEnglish(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = steve::strlenUtf8(g_poemEnglish);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_Steve_strlenUtf8_PoemChinese(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = steve::strlenUtf8(g_poemChinese);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_Steve_strlenUtf8_PoemRussian(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = steve::strlenUtf8(g_poemRussian);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_AxCut_utf8StringLength_Random(benchmark::State& state) {
     for (auto _ : state) {
         auto result = axcut::utf8StringLength(test_string);
         benchmark::DoNotOptimize(result);
     }
 }
 
-BENCHMARK(BM_Steve_strlenUtf8_Traditional);
-BENCHMARK(BM_Steve_StrlenUtf8);
-BENCHMARK(BM_AxCut_utf8StringLength);
+static void BM_AxCut_utf8StringLength_PoemEnglish(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = axcut::utf8StringLength(g_poemEnglish);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_AxCut_utf8StringLength_PoemChinese(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = axcut::utf8StringLength(g_poemChinese);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+static void BM_AxCut_utf8StringLength_PoemRussian(benchmark::State& state) {
+    for (auto _ : state) {
+        auto result = axcut::utf8StringLength(g_poemRussian);
+        benchmark::DoNotOptimize(result);
+    }
+}
+
+BENCHMARK(BM_Steve_strlenUtf8_Traditional_Random);
+BENCHMARK(BM_Steve_strlenUtf8_Traditional_PoemEnglish);
+BENCHMARK(BM_Steve_strlenUtf8_Traditional_PoemChinese);
+BENCHMARK(BM_Steve_strlenUtf8_Traditional_PoemRussian);
+
+BENCHMARK(BM_Steve_strlenUtf8_Random);
+BENCHMARK(BM_Steve_strlenUtf8_PoemEnglish);
+BENCHMARK(BM_Steve_strlenUtf8_PoemChinese);
+BENCHMARK(BM_Steve_strlenUtf8_PoemRussian);
+
+BENCHMARK(BM_AxCut_utf8StringLength_Random);
+BENCHMARK(BM_AxCut_utf8StringLength_PoemEnglish);
+BENCHMARK(BM_AxCut_utf8StringLength_PoemChinese);
+BENCHMARK(BM_AxCut_utf8StringLength_PoemRussian);
 
 //BENCHMARK_MAIN();
 
--- strings.hpp
+++ strings.hpp
@@ -4,3 +4,7 @@
 #include <string>
 
 std::string make_random_utf8(size_t target_bytes);
+
+extern std::string g_poemEnglish;
+extern std::string g_poemChinese;
+extern std::string g_poemRussian;

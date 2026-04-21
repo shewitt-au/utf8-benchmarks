a55e864f16695523e7449c59c28d9ba8a0150fcf
shewitt.au <shewitt.au@gmail.com>
2026-04-22 04:17:55 +1000

    Typo

--- bench.cpp
+++ bench.cpp
@@ -176,7 +176,7 @@ BENCHMARK(BM_AxCut_utf8StringLength2_PoemRussian)
 //BENCHMARK_MAIN();
 
 int main(int argc, char** argv) {
-    std::cout << "Benchmark randoms string\n";
+    std::cout << "Benchmark random string\n";
     std::cout << "----------------\n";
     std::cout << test_string << '\n';
     std::cout << "----------------\n";
